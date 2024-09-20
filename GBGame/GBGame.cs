a8ae09fe7970c76872c765866d40ca25c317c809
its-Lyn <143884194+its-Lyn@users.noreply.github.com>
2024-09-20 20:19:57 +0300

    Initialise project.

---
+++ GBGame/GBGame.cs
@@ -0,0 +1,2 @@
+﻿using var game = new GBGame.GameWindow();
+game.Run();
---
+++ GBGame/GameWindow.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGayme.Utilities;
+
+namespace GBGame;
+
+public class GameWindow : Game
+{
+    private GraphicsDeviceManager _graphics = null!;
+    private SpriteBatch _spriteBatch = null!;
+
+    private Renderer _renderer;
+
+    public Vector2 GameSize { get; private set;}
+
+    public GameWindow()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+
+        GameSize = new Vector2(160, 144);
+        _graphics.SetWindowSize(GameSize * 3);
+
+        _renderer = new Renderer(GameSize, GraphicsDevice); 
+
+        Window.AllowUserResizing = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        _renderer.SetRenderer();
+        GraphicsDevice.Clear(Color.White);
+
+        _renderer.DrawRenderer(_spriteBatch);
+
+        base.Draw(gameTime);
+    }
+}
